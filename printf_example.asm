2d42d3fb9dd7ff92cd5385ba1eb5f77e55cd37b9
oinkina <lambdaoinks@gmail.com>
2014-11-09 01:40:44 -0800

    comments on printf_ex

--- printf_example.asm
+++ printf_example.asm
@@ -2,16 +2,16 @@ format ELF64
 include "syscalls.inc"
 public start
 
-extrn printf
+extrn printf    ; extrn is copublic
 
 start:
-  and rsp, (not 0x0f)
+  and rsp, (not 1111b) ; for calling c fns, need to align rsp
   lea rdi, [msg]
   mov rsi, 42
   call printf
 
-	xor	edi,edi
-	mov	eax, SYSCALL_EXIT
+	xor	edi,edi          ; 0 is arg for exit (ie success)
+	mov eax, SYSCALL_EXIT
 	syscall
 
 msg db 'The number is: %d',0xA,0x0
